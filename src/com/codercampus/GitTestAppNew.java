489fd47fd7c70d99521a65235bd344e58d8b38a8
mahribangit <144757982+mahribangit@users.noreply.github.com>
2023-09-25 15:43:53 -0500

    Initial commit

---
+++ src/com/codercampus/GitTestAppNew.java
@@ -0,0 +1,11 @@
+package com.codercampus;
+
+public class GitTestAppNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is JUST a New Git Test");
+
+	}
+
+}
